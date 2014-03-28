c22f0db958e6797bf4476722c0df18a9fb287a30
Falk Doering <falk.doering@fadoe.de>
2014-03-28 19:22:32 +0100

    add request and response objects

--- config/application.config.php
+++ config/application.config.php
@@ -4,6 +4,8 @@ return array(
     'service_manager' => array(
         'invokables' => array(
             'Sfive\Entity\Author' => 'Sfive\Entity\Author',
+            'Request' => 'Zend\Http\PhpEnvironment\Request',
+            'Response' => 'Zend\Http\PhpEnvironment\Response',
         ),
         'factories' => array(
             'Sfive\Repository\Author' => 'Sfive\Repository\AuthorFactory',
